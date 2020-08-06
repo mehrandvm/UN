b6455616eb8c59d1aa2e2578be5084059829f72e
M. M. Mahmoodian <mahmoodian.m1999@gmail.com>
2020-08-06 16:26:22 +0430

    [ADD] adding some field to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('mobile_number')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('username');
+            $table->string('photo')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('last_login')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
