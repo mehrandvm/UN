96a74e519abad5fac496cbed2e6c19bca606f799
M. M. Mahmoodian <mahmoodian.m1999@gmail.com>
2020-08-10 04:57:28 +0430

    [ADD] creating the relationships between roles and permissions models

--- app/Permission.php
+++ app/Permission.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'roles_join_permissions');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'users_join_permissions');
+    }
 }
