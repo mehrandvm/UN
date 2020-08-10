e2784de7d61dbf3d285d5863e9a9e720fc067468
M. M. Mahmoodian <mahmoodian.m1999@gmail.com>
2020-08-10 04:50:07 +0430

    [MOD] 'join' keyword added to table name

--- database/migrations/2020_08_10_001540_create_users_join_permissions_table.php
+++ database/migrations/2020_08_10_001540_create_users_join_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersPermissionsTable extends Migration
+class CreateUsersJoinPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersPermissionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('users_permissions', function (Blueprint $table) {
+        Schema::create('users_join_permissions', function (Blueprint $table) {
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('permission_id');
 
@@ -33,6 +33,6 @@ class CreateUsersPermissionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_permissions');
+        Schema::dropIfExists('users_join_permissions');
     }
 }
