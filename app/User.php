c005074795bf1ffa1d60cdfc23e9a6fe02b6f60a
M. M. Mahmoodian <mahmoodian.m1999@gmail.com>
2020-08-10 05:03:25 +0430

    [ADD] trait has been setup to handle user relations

--- app/User.php
+++ app/User.php
@@ -7,9 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
+use App\Permissions\HasPermissionsTrait;
+
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, HasPermissionsTrait;
 
     /**
      * The attributes that are mass assignable.
