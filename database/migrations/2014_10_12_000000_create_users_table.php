4a611c4f0f30e16451f497035e4778f97c99a2cb
M. M. Mahmoodian <mahmoodian.m1999@gmail.com>
2020-08-10 04:52:25 +0430

    [ADD] migration for users join roles added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('f_name');
             $table->string('l_name');
             $table->string('phone_number')->unique();
