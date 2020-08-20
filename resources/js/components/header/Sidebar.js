3970023ab185f4d0fce916b2c56f0ed7ad916096
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-08-20 20:39:41 +0430

    [ADD] added sidebar in case of need

---
+++ resources/js/components/header/Sidebar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Link from '@material-ui/core/Link';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import MapIcon from '@material-ui/icons/Map';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const useStyles = makeStyles({
+    list: {
+        width: 250,
+    },
+    link: {
+        textDecoration: 'none',
+        color: '#000',
+    },
+});
+
+const Sidebar = (props) => {
+    const classes = useStyles();
+    const sidebarItems = [
+        { title: "Home", link: "/", icon: <MapIcon /> },
+        { title: "Login", link: "/login", icon: <LocationOnIcon /> },
+    ]
+    const list = () => (
+        <div
+            onClick={props.closeDrawer}
+            onKeyDown={props.closeDrawer}
+        >
+            <List className={classes.list}>
+                {sidebarItems.map((item) => (
+                    <Link href={`${item.link}`} key={item.title}><ListItem button className={classes.link}>
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText primary={item.title} />
+                    </ListItem></Link>
+                ))}
+            </List>
+        </div>
+    );
+
+    return (
+        <React.Fragment>
+            <SwipeableDrawer
+                anchor={'left'}
+                open={props.open}
+                onClose={props.closeDrawer}
+                onOpen={props.openDrawer}
+            >
+                {list()}
+            </SwipeableDrawer>
+        </React.Fragment>
+    );
+}
+
+export default Sidebar
