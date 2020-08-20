3ba24be897307a84219917a64d654d0ddb4327fa
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-08-20 20:36:41 +0430

    [ADD] added theme component

---
+++ resources/js/components/theme/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { blue, amber } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: amber,
+  },
+});
+
+export default theme
