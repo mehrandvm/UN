bf723211108819db494928a557fcdc893171dea8
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-08-20 20:41:42 +0430

    [ADD] added home blade view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,19 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title>Systana</title>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="container">
+                <div id="home" csrf_token='{{csrf_token()}}' auth_message='{{ $message ?? '' }}'></div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+        <script src="/js/app.js"></script>
+    </body>
+</html>
