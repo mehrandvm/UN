cc3ca912b994b0c1789cbc1cd82caa110e9f92a4
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-08-27 18:56:30 +0430

    [MOD] added new components to map dashboard

--- resources/js/components/dashboard/Dashboard.js
+++ resources/js/components/dashboard/Dashboard.js
@@ -5,6 +5,12 @@ import ChartSelector from "../charts/ChartSelector";
 import Header from "../header/Header";
 import data from "./mock";
 import MapFragment from "../map/MapFragment";
+import HazardSelector from "../map/HazardSelector";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import DivisionSelector from "../map/DivisionSelector";
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -14,6 +20,7 @@ const useStyles = makeStyles(theme => ({
     },
     mapContainer: {
         textAlign: "center",
+        paddingTop: 20,
     },
     chartContainer: {
         overflow: "hidden",
@@ -28,29 +35,44 @@ const useStyles = makeStyles(theme => ({
     mapTitle: {
         textAlign: 'center'
     },
+    formControl: {
+        width: '100%',
+    },
 }));
 
 const Dashboard = () => {
     const [mapParams, setMapParams] = useState();
     const [mapTitle, setMapTitle] = useState('');
     const [mapRightStats, setMapRightStats] = useState([]);
+    const [mapBottomStats, setMapBottomStats] = useState([]);
+    const [selectedDivision, setSelectedDivision] = useState([]);
+    const [mapDivisionLevel, setMapDivisionLevel] = useState(1);
+    const [mapHazards, setMapHazards] = useState({
+        hazardOne: false,
+        hazardTwo: false,
+        hazardThree: false,
+    });
     useEffect(() => {
         const mapData = data;
         setMapParams(mapData.mapParams);
         setMapTitle(mapData.title);
         setMapRightStats(Object.values(mapData.rightCharts));
+        setMapBottomStats(Object.values(mapData.bottomCharts));
     }, [])
-    console.log(data);
+    // console.log(data);
+    const handleMapDivisionLevel = (e) => {
+        setMapDivisionLevel(e.target.value);
+    }
     const classes = useStyles();
     return (
         <>
             <Header/>
             <div className={classes.container}>
-                <Box p={1} className={classes.mapTitle}>
-                    <Typography variant='h5'>
-                        {`IPPO - RPW Contain & Eradicion Program (${mapTitle} View)`}
-                    </Typography>
-                </Box>
+                {/*<Box p={1} className={classes.mapTitle}>*/}
+                {/*    <Typography variant='h5'>*/}
+                {/*        {`IPPO - RPW Contain & Eradicion Program (${mapTitle} View)`}*/}
+                {/*    </Typography>*/}
+                {/*</Box>*/}
                 <Grid
                     container
                     direction="row"
@@ -63,12 +85,45 @@ const Dashboard = () => {
                         sm={12}
                         md={8}
                         className={classes.mapContainer}
+                        spacing={2}
                     >
+                        <Grid item xs={4}>
+                            <HazardSelector mapHazards={mapHazards} setMapHazards={setMapHazards} />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <FormControl variant="outlined" className={classes.formControl}>
+                                <InputLabel>Division Level</InputLabel>
+                                <Select
+                                    value={mapDivisionLevel}
+                                    onChange={handleMapDivisionLevel}
+                                    label="Select Division Level"
+                                >
+                                    <MenuItem value={1}>National</MenuItem>
+                                    <MenuItem value={2}>Province</MenuItem>
+                                    <MenuItem value={3}>City</MenuItem>
+                                    <MenuItem value={4}>Village</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <DivisionSelector
+                                selectedDivision={selectedDivision}
+                                setSelectedDivision={setSelectedDivision}
+                            />
+                        </Grid>
                         <Grid item xs={12}>
                             {mapParams ?
                                 <MapFragment height={0.7} params={mapParams}/>
                                 : null}
                         </Grid>
+                        {mapBottomStats.map((chart, i) => {
+                                return (
+                                    <Grid item xs={12} key={i}>
+                                        <ChartSelector chart={chart}/>
+                                    </Grid>
+                                )
+                            }
+                        )}
                     </Grid>
                     <Grid
                         container
