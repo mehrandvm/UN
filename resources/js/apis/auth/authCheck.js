c6eb96c642dc9f6d41d0d853c543014c05cc3dab
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-09-02 00:11:33 +0430

    [UPD] disabled check auth function

--- resources/js/apis/auth/authCheck.js
+++ resources/js/apis/auth/authCheck.js
@@ -1,15 +1,16 @@
-import {AxiosInstance as axiosInstance} from "axios";
+import axiosInstance from '../AxiosConfig'
 
 export const loginAPI = async (email, password) => axiosInstance.post(
     '/user/login', {email, password}
     );
 
 export const checkAuth = async () => {
-    console.log('Log: Check Auth Called!')
-    try {
-        await axiosInstance.get('/user');
-        return true;
-    } catch (e) {
-        return false;
-    }
+    // console.log('Log: Check Auth Called!')
+    // try {
+    //     await axiosInstance.get('/user');
+    //     return true;
+    // } catch (e) {
+    //     return false;
+    // }
+    return true
 };
