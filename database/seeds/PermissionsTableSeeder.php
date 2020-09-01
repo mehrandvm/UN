b8f591a0b7b2c2e180762c4a65ccd2c1884a678b
M. M. Mahmoodian <mahmoodian.m1999@gmail.com>
2020-09-01 20:27:25 +0430

    [ADD] permissions added to seeds

---
+++ database/seeds/PermissionsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Permission;
+
+class PermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $manageUsers = new Permission();
+		$manageUsers->slug = 'manage-users';
+		$manageUsers->name = 'Manage Users';
+        $manageUsers->save();
+
+        $manageRoles = new Permission();
+		$manageRoles->slug = 'manage-roles';
+		$manageRoles->name = 'Manage Roles';
+        $manageRoles->save();
+
+        $manageTasks = new Permission();
+		$manageTasks->slug = 'manage-tasks';
+		$manageTasks->name = 'Manage Tasks';
+        $manageTasks->save();
+
+        $viewDashboard = new Permission();
+		$viewDashboard->slug = 'view-dashboard';
+		$viewDashboard->name = 'View Dashboard';
+        $viewDashboard->save();
+    }
+}
