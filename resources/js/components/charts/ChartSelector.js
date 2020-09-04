e81683c55a0cf0c1702acf02f125a262d6d0c2ad
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-09-04 09:40:44 +0430

    [UPD] passed theme to bar chart in chart selector

--- resources/js/components/charts/ChartSelector.js
+++ resources/js/components/charts/ChartSelector.js
@@ -1,49 +1,49 @@
 import React from "react";
 import PieChart from "./PieChart";
 import BarChart from "./BarChart";
-import { Grid, Typography } from "@material-ui/core";
+import {Grid, Typography} from "@material-ui/core";
 
 
 const ChartSelector = (props) => {
 
-  const returnValidData = (dataset) => {
-    const validData = { labels: Object.keys(dataset), dataset: Object.values(dataset), }
-    return validData
-  }
-
-  const returnValidMultiData = (dataset) => {
-    const validData = { labels: dataset.labels, datasets: dataset.datasets }
-    return validData
-  }
-
-  const renderChart = (chart) => {
-    switch (chart.type) {
-      case 'pie':
-        return <PieChart data={returnValidData(chart.dataset)} />
-      case 'bar':
-        return <BarChart data={returnValidMultiData(chart)} />
-      default:
-        return null;
+    const returnValidData = (dataset) => {
+        const validData = {labels: Object.keys(dataset), dataset: Object.values(dataset),}
+        return validData
     }
-  }
 
-  return (
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      style={{ textAlign: 'center' }}
-    >
-      {props.chart.type === 'stats'
-        || props.chart.type === 'area'
-        || props.chart.type === 'temp' ? null : <Grid item xs={12}>
-          <Typography variant='body1'>{props.chart.title}</Typography>
-        </Grid>}
-      <Grid item xs={12}>
-        {renderChart(props.chart)}
-      </Grid>
-    </Grid >
-  );
+    const returnValidMultiData = (dataset) => {
+        const validData = {labels: dataset.labels, datasets: dataset.datasets, theme: dataset.theme}
+        return validData
+    }
+
+    const renderChart = (chart) => {
+        switch (chart.type) {
+            case 'pie':
+                return <PieChart data={returnValidData(chart.dataset)}/>
+            case 'bar':
+                return <BarChart data={returnValidMultiData(chart)}/>
+            default:
+                return null;
+        }
+    }
+
+    return (
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            style={{textAlign: 'center',}}
+        >
+            {props.chart.type === 'stats'
+            || props.chart.type === 'area'
+            || props.chart.type === 'temp' ? null : <Grid item xs={12}>
+                <Typography variant='body1'>{props.chart.title}</Typography>
+            </Grid>}
+            <Grid item xs={12}>
+                {renderChart(props.chart)}
+            </Grid>
+        </Grid>
+    );
 };
 
 export default ChartSelector
