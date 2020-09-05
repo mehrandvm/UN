09a3866addf8c3d2359a23e297ce6e95d0af406e
M. M. Mahmoodian <mahmoodian.m1999@gmail.com>
2020-09-05 18:50:50 +0430

    [MOD] change some fields and run order in country subdivision migrations

--- database/migrations/2020_09_06_103755_create_country_subdivisions_table.php
+++ database/migrations/2020_09_06_103755_create_country_subdivisions_table.php
@@ -14,7 +14,7 @@ class CreateCountrySubdivisionsTable extends Migration
     public function up()
     {
         Schema::create('country_subdivisions', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->increments('id');
             $table->string('subdivision_name');
             $table->integer('amar_village_code');
             $table->integer('household_count')->nullable();
@@ -23,11 +23,15 @@ class CreateCountrySubdivisionsTable extends Migration
             $table->integer('males_count')->nullable();
             $table->integer('building_unit_count')->nullable();
             $table->boolean('is_residency_permanent')->nullable();
+
             $table->unsignedInteger('roof_type_id')->nullable();
             $table->foreign('roof_type_id')->references('id')->on('roof_types');
+
             $table->unsignedInteger('cathedra_type_id')->nullable();
             $table->foreign('cathedra_type_id')->references('id')->on('cathedra_types');
+
             $table->boolean('is_one_story')->nullable();
+
             $table->unsignedInteger('foudation_construction_material_id')->nullable();
             $table->foreign('foudation_construction_material_id')->references('id')->on('construction_materials');
             $table->unsignedInteger('walls_construction_material_id')->nullable();
@@ -36,11 +40,16 @@ class CreateCountrySubdivisionsTable extends Migration
             $table->foreign('roof_construction_material_id')->references('id')->on('construction_materials');
             $table->unsignedInteger('window_construction_material_id')->nullable();
             $table->foreign('window_construction_material_id')->references('id')->on('construction_materials');
+
             // $table->integer('unique_code');
+            $table->unsignedInteger('type');
             $table->foreign('type')->references('id')->on('country_subdivision_types');
+
             $table->unsignedInteger('parent_id')->nullable();
             $table->foreign('parent_id')->references('id')->on('country_subdivisions');
-            $table->polygon('area')->nullable();
+
+            // $table->polygon('area')->nullable();
+            $table->unsignedInteger('controlled_by_admin');
             $table->foreign('controlled_by_admin')->references('id')->on('users');
             $table->timestamps();
         });
