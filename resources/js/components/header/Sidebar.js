7f9a3ccb4ff8165dae9af2b7bbcd50acc858826c
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-09-05 08:00:09 +0430

    [UPD] updated side bar routes

--- resources/js/components/header/Sidebar.js
+++ resources/js/components/header/Sidebar.js
@@ -7,7 +7,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import MapIcon from '@material-ui/icons/Map';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import HomeIcon from '@material-ui/icons/Home';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
@@ -25,6 +25,7 @@ const useStyles = makeStyles({
 const Sidebar = (props) => {
     const classes = useStyles();
     const sidebarItems = [
+        { title: "Home", link: "/", icon: <HomeIcon /> },
         { title: "Dashboard", link: "/dashboard", icon: <DashboardIcon /> },
         { title: "Users", link: "/user", icon: <SupervisorAccountIcon /> },
         { title: "Roles", link: "/", icon: <AssignmentIndIcon /> },
