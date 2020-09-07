e98364bde7a859cd97052f1b56e783eef9e81115
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-09-07 11:21:09 +0430

    [UPD] created county selector

---
+++ resources/js/components/map/CountySelector.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import countyFeatureCollection from '../../../static/county.json'
+import axiosInstance from "../../apis/AxiosConfig";
+
+function sleep(delay = 0) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay);
+    });
+}
+
+const CountySelector = (props) => {
+    const [open, setOpen] = React.useState(false);
+    const [options, setOptions] = React.useState([]);
+    const {selectedDivision, setSelectedDivision, divisionLevel, setDivisionLevel, clearCounty} = props;
+    const loading = open && (selectedDivision === null || selectedDivision.length === 0);
+
+
+    const isDisabled = () => divisionLevel === "national" || divisionLevel === "none"
+    const handleCountyChange = () => setDivisionLevel("county")
+
+    React.useEffect(() => {
+        let active = true;
+
+        if (!loading) {
+            return undefined;
+        }
+
+        (async () => {
+            // const response = await Axios.get('https://country.register.gov.uk/records.json?page-size=5000');
+            await sleep(1e3); // For demo purposes.
+            // const countries = await response.json();
+
+            if (active) {
+                // await axiosInstance.get('http://194.5.188.215:8080/geoserver/UN/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=UN%3AK_Villages&outputFormat=application%2Fjson'
+                // ).then((res) => console.log(res))
+                setOptions(countyFeatureCollection.features.map((feature) => feature.properties.Name_En))
+                // setOptions(Object.keys(countries).map((key) => countries[key].item[0]));
+            }
+        })();
+
+        return () => {
+            active = false;
+        };
+    }, [loading]);
+
+    React.useEffect(() => {
+        if (!open) {
+            setOptions([]);
+        }
+    }, [open]);
+
+    return (
+        <Autocomplete
+            style={{width: '100%'}}
+            open={open}
+            onOpen={() => {
+                setOpen(true);
+            }}
+            onClose={() => {
+                setOpen(false);
+            }}
+            getOptionSelected={(option, value) => option === value}
+            getOptionLabel={(option) => option}
+            options={options}
+            loading={loading}
+            disabled={isDisabled()}
+            value={selectedDivision}
+            onChange={(event, newValue) => {
+                setSelectedDivision(newValue)
+                if (newValue===null){clearCounty()}
+                else{setDivisionLevel("county")}
+            }}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Selected County"
+                    variant="outlined"
+                    InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <React.Fragment>
+                                {loading ? <CircularProgress color="inherit" size={20}/> : null}
+                                {params.InputProps.endAdornment}
+                            </React.Fragment>
+                        ),
+                    }}
+                />
+            )}
+        />
+    );
+}
+
+export default CountySelector
