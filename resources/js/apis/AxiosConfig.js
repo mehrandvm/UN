f72aae385c230b1e6c9caa96785b63f31f72f89d
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-09-12 12:05:27 +0430

    [UPD] added bearer token to axios interceptors

--- resources/js/apis/AxiosConfig.js
+++ resources/js/apis/AxiosConfig.js
@@ -37,9 +37,9 @@ export const removeAxiosHeader = (key) => {
     if (key in axiosInstance.defaults.headers) delete axiosInstance.defaults.headers[key];
 };
 
-// export const deleteAxiosInstanceHeaders = (headerName) => {
-//     delete axiosInstance.defaults.headers[headerName];
-// };
+export const deleteAxiosInstanceHeaders = (headerName) => {
+    delete axiosInstance.defaults.headers[headerName];
+};
 
 // to check if a request needs authorization,
 // we need to specify routes who needs authorization
@@ -49,38 +49,39 @@ export const removeAxiosHeader = (key) => {
 // then we go on making those request else we drop those
 // requests
 
-// const needPermission = [{ url: '/panel', exact: false }];
-//
-// const reqNeedsPermission = (url : string) => needPermission.some((item) => (
-//     item.exact ? url === item.url : url.includes(item.url)));
-//
-// axiosInstance.interceptors.request.use((request : AxiosRequestConfig) => {
-//     if (request.url && reqNeedsPermission(request.url)) {
-//         if (!request.headers.Authorization) {
-//             throw new axios.Cancel('no-default-auth');
-//         }
-//     }
-//     return request;
-// }, (error : AxiosError) => Promise.reject(error));
-//
-// axiosInstance.interceptors.response.use((response) => response, (error) => {
-//     let msg = 'error';
-//     if (error.response) {
-//         if (error.response.data) {
-//             if (typeof error.response.data.message === 'string') {
-//                 msg = error.response.data.message;
-//             } else if (error.response.data.error) {
-//                 msg = error.response.data.error;
-//             }
-//         }
-//     } else if (error.message) {
-//         msg = error.message;
-//     }
-//     if (msg === 'no-default-auth') {
-//         return Promise.reject(error);
-//     }
-//     SnackbarUtil.error(msg);
-//     return Promise.reject(error);
-// });
+const needPermission = [{ url: '/panel', exact: false }];
+
+const reqNeedsPermission = (url ) => needPermission.some((item) => (
+    item.exact ? url === item.url : url.includes(item.url)));
+
+axiosInstance.interceptors.request.use((request ) => {
+    if (request.url && reqNeedsPermission(request.url)) {
+        if (!request.headers.Authorization) {
+            // throw new axios.Cancel('no-default-auth');
+        }
+    }
+    request.headers.Authorization =  `Bearer ${localStorage.getItem(tokenTitle)}`;
+    return request;
+}, (error ) => Promise.reject(error));
+
+axiosInstance.interceptors.response.use((response) => response, (error) => {
+    let msg = 'error';
+    if (error.response) {
+        if (error.response.data) {
+            if (typeof error.response.data.message === 'string') {
+                msg = error.response.data.message;
+            } else if (error.response.data.error) {
+                msg = error.response.data.error;
+            }
+        }
+    } else if (error.message) {
+        msg = error.message;
+    }
+    if (msg === 'no-default-auth') {
+        return Promise.reject(error);
+    }
+    // SnackbarUtil.error(msg);
+    return Promise.reject(error);
+});
 
 export default axiosInstance;
