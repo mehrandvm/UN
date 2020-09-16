fb0a62c8ed80000dd90c596a66de1a65cee050b8
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-09-16 18:23:53 +0430

    [ADD] added language context and rtl support to index

--- resources/js/index.js
+++ resources/js/index.js
@@ -7,19 +7,28 @@ import Panel from "./components/panel/Panel";
 import Login from "./components/login/Login";
 import Home from "./components/home/Home";
 import {ThemeProvider} from "@material-ui/core";
-import theme from "./components/theme/theme";
+import theme, {themeRTL} from "./components/theme/theme";
 import UserForm from "./components/user-form/UserForm";
 import Dashboard from "./components/dashboard/Dashboard";
 import {removeAxiosHeader, setAxiosHeader, tokenTitle} from "./apis/AxiosConfig";
 import {checkAuth, loginAPI} from "./apis/auth/authCheck";
 import {LoginContext} from "./contexts/login-context/LoginContext";
+import {LanguageContext} from "./contexts/language-context/LanguageContext";
 import {SnackbarProvider} from 'notistack';
 import Task from "./components/task/Task";
 import TaskForm from "./components/task-form/TaskForm";
 import MyTasks from "./components/my-tasks/MyTasks";
+import {create} from 'jss';
+import rtl from 'jss-rtl';
+import {StylesProvider, jssPreset} from '@material-ui/core/styles';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import {getTranslator} from "./vocabs";
+
+const jss = create({plugins: [...jssPreset().plugins, rtl()]});
 
 const Index = () => {
     const [token, setToken] = React.useState(localStorage.getItem(tokenTitle) || null);
+    const [language, setLanguage] = React.useState(localStorage.getItem('language') ||'en');
 
     setAxiosHeader('Content-Type', `application/json`);
 
@@ -57,48 +66,60 @@ const Index = () => {
     }, [setLoginToken]);
 
     return (
-        <ThemeProvider theme={theme}>
-            <LoginContext.Provider value={{token, setToken: setLoginToken, login}}>
-                <SnackbarProvider maxSnack={3}>
-                    <Router>
-                        <Switch>
-                            <Route exact path="/user/edit/:id">
-                                <UserForm/>
-                            </Route>
-                            <Route exact path="/user/new">
-                                <UserForm/>
-                            </Route>
-                            <Route exact path="/user">
-                                <User/>
-                            </Route>
-                            <Route exact path="/mytasks">
-                                <MyTasks/>
-                            </Route>
-                            <Route exact path="/task/new">
-                                <TaskForm/>
-                            </Route>
-                            <Route exact path="/task">
-                                <Task/>
-                            </Route>
-                            {/*<Route exact path="/role">*/}
-                            {/*    <Role/>*/}
-                            {/*</Route>*/}
-                            <Route exact path="/dashboard">
-                                <Dashboard/>
-                            </Route>
-                            {/*<Route exact path="/panel">*/}
-                            {/*    <Panel/>*/}
-                            {/*</Route>*/}
-                            <Route exact path="/login">
-                                <Login/>
-                            </Route>
-                            <Route path="/">
-                                <Home/>
-                            </Route>
-                        </Switch>
-                    </Router>
-                </SnackbarProvider>
-            </LoginContext.Provider>
+        <ThemeProvider theme={language === "en" ? theme : themeRTL}>
+            <StylesProvider jss={jss}>
+                <CssBaseline/>
+                <LanguageContext.Provider value={{
+                    language,
+                    changeLanguage: setLanguage,
+                    translator: getTranslator(language),
+                }}
+                >
+                    <LoginContext.Provider value={{token, setToken: setLoginToken, login}}>
+                        <SnackbarProvider maxSnack={3}>
+                            <div dir={language==='en'? 'ltr' : 'rtl'}>
+                                <Router>
+                                    <Switch>
+                                        <Route exact path="/user/edit/:id">
+                                            <UserForm/>
+                                        </Route>
+                                        <Route exact path="/user/new">
+                                            <UserForm/>
+                                        </Route>
+                                        <Route exact path="/user">
+                                            <User/>
+                                        </Route>
+                                        <Route exact path="/mytasks">
+                                            <MyTasks/>
+                                        </Route>
+                                        <Route exact path="/task/new">
+                                            <TaskForm/>
+                                        </Route>
+                                        <Route exact path="/task">
+                                            <Task/>
+                                        </Route>
+                                        {/*<Route exact path="/role">*/}
+                                        {/*    <Role/>*/}
+                                        {/*</Route>*/}
+                                        <Route exact path="/dashboard">
+                                            <Dashboard/>
+                                        </Route>
+                                        {/*<Route exact path="/panel">*/}
+                                        {/*    <Panel/>*/}
+                                        {/*</Route>*/}
+                                        <Route exact path="/login">
+                                            <Login/>
+                                        </Route>
+                                        <Route path="/">
+                                            <Home/>
+                                        </Route>
+                                    </Switch>
+                                </Router>
+                            </div>
+                        </SnackbarProvider>
+                    </LoginContext.Provider>
+                </LanguageContext.Provider>
+            </StylesProvider>
         </ThemeProvider>
     );
 }
