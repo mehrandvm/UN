359cda079afae3b3be89655af09b575678a5250e
M. M. Mahmoodian <mahmoodian.m1999@gmail.com>
2020-10-03 19:14:21 +0330

    [ADD] method for creating question, category and survey form added

--- app/Http/Controllers/api/v1/IncidentController.php
+++ app/Http/Controllers/api/v1/IncidentController.php
@@ -2,10 +2,90 @@
 
 namespace App\Http\Controllers\api\v1;
 
+use App\Category;
 use App\Http\Controllers\Controller;
+use App\Question;
+use App\SurveyForm;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class IncidentController extends Controller
 {
-    //
+    public $successStatus = 200;
+    public $forbiddenStatus = 403;
+    public $notFoundStatus = 404;
+    public $badRequestStatus = 404;
+
+
+    public function addSurveyForm(Request $request){
+        $user = Auth::user(); 
+        
+        $validator = Validator::make($request->all(),[ 
+            'name' => 'required', 
+            'incident' => 'required', 
+        ]);
+        if ($validator->fails()) { 
+            return response()->json([
+                'status_code' => $this->badRequestStatus,
+                'status_message' => 'Bad request',
+                'error'=>$validator->errors()
+            ]);          
+        }else{
+            $input = $request->all(); 
+            $target = SurveyForm::create($input);
+            return response()->json([
+                'status_code' => $this->successStatus,
+                'status_message' => 'New Survey Form Successfully Created',
+            ]);
+        }
+    }
+
+    public function addCategory(Request $request){
+        $user = Auth::user(); 
+        
+        $validator = Validator::make($request->all(),[ 
+            'name' => 'required', 
+            'weight' => 'required',
+            'form' => 'required', 
+        ]);
+        if ($validator->fails()) { 
+            return response()->json([
+                'status_code' => $this->badRequestStatus,
+                'status_message' => 'Bad request',
+                'error'=>$validator->errors()
+            ]);          
+        }else{
+            $input = $request->all(); 
+            $target = Category::create($input);
+            return response()->json([
+                'status_code' => $this->successStatus,
+                'status_message' => 'New Category Successfully Created',
+            ]);
+        }
+    }
+
+    public function addCategory(Request $request){
+        $user = Auth::user(); 
+        
+        $validator = Validator::make($request->all(),[ 
+            'text' => 'required', 
+            'rate' => 'required',
+            'category' => 'required', 
+        ]);
+        if ($validator->fails()) { 
+            return response()->json([
+                'status_code' => $this->badRequestStatus,
+                'status_message' => 'Bad request',
+                'error'=>$validator->errors()
+            ]);          
+        }else{
+            $input = $request->all(); 
+            $target = Question::create($input);
+            return response()->json([
+                'status_code' => $this->successStatus,
+                'status_message' => 'New Question Successfully Created',
+            ]);
+        }
+    }
 }
