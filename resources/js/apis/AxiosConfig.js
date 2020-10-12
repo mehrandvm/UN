0c060b4b68567acd78cc567935e587609b3cf061
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-10-12 16:09:22 +0330

    [UPD] added try catch to JWT function

--- resources/js/apis/AxiosConfig.js
+++ resources/js/apis/AxiosConfig.js
@@ -2,13 +2,18 @@ import axios from 'axios';
 // import SnackbarUtil from "./components/SnackbarUtil/SnackbarUtil";
 
 export const parseJwt = (token) => {
-    let base64Url = token.split('.')[1];
-    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+    try {
+        let base64Url = token.split('.')[1];
+        let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
 
-    return JSON.parse(jsonPayload);
+        return JSON.parse(jsonPayload);
+    } catch (e) {
+        console.log('Invalid JWT Token')
+        return ''
+    }
 };
 
 export const tokenTitle = 'token';
