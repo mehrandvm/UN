3a1f28eac5b02a2e605d4d1b4079c4d444050361
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-10-28 08:05:34 +0330

    [UPD] reworked hazard selector into a select

--- resources/js/components/map/HazardSelector.js
+++ resources/js/components/map/HazardSelector.js
@@ -9,35 +9,47 @@ import {makeStyles} from "@material-ui/core/styles";
 import Checkbox from "@material-ui/core/Checkbox";
 import {getTranslator} from "../../vocabs";
 import {LanguageContext} from "../../contexts/language-context/LanguageContext";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
 
 const useStyles = makeStyles(theme => ({
     container: {
         backgroundColor: 'rgb(255,255,255,0.6)',
         borderRadius: 10,
-        paddingLeft: 10,
-        margin: 5,
+        width: '100%'
     },
 }));
 
 const HazardSelector = (props) => {
     const handleChange = (event) => {
-        props.setHazards({...props.hazards, [event.target.name]: event.target.checked});
+        console.log(event.target.value)
+        props.setHazards(event.target.value);
+        // props.setHazards({...props.hazards, [event.target.name]: event.target.checked});
     };
     const classes = useStyles()
     const vocabs = getTranslator(useContext(LanguageContext).language);
     return (
-        <FormControl component="fieldset" className={classes.container}>
-            {/*<FormLabel component="legend">Assign responsibility</FormLabel>*/}
-            <FormGroup>
-                <FormControlLabel
-                    control={<Checkbox checked={props.hazards.seismicHazard} onChange={handleChange} name={'seismicHazard'} />}
-                    label={vocabs('seismic-hazard-zone')}
-                />
-                <FormControlLabel
-                    control={<Checkbox checked={props.hazards.floodHazard} onChange={handleChange} name={'floodHazard'} />}
-                    label={vocabs('flood-hazard-zone')}
-                />
-            </FormGroup>
+        <FormControl component="fieldset" variant="outlined" className={classes.container}>
+            <InputLabel>{vocabs('hazard-placeholder')}</InputLabel>
+            <Select
+                value={props.hazards}
+                onChange={handleChange}
+            >
+                <MenuItem value={''}>{vocabs('none')}</MenuItem>
+                <MenuItem value={'seismicHazard'}>{vocabs('seismic-hazard-zone')}</MenuItem>
+                <MenuItem value={'floodHazard'}>{vocabs('flood-hazard-zone')}</MenuItem>
+            </Select>
+            {/*<FormGroup>*/}
+            {/*    <FormControlLabel*/}
+            {/*        control={<Checkbox checked={props.hazards.seismicHazard} onChange={handleChange} name={'seismicHazard'} />}*/}
+            {/*        label={vocabs('seismic-hazard-zone')}*/}
+            {/*    />*/}
+            {/*    <FormControlLabel*/}
+            {/*        control={<Checkbox checked={props.hazards.floodHazard} onChange={handleChange} name={'floodHazard'} />}*/}
+            {/*        label={vocabs('flood-hazard-zone')}*/}
+            {/*    />*/}
+            {/*</FormGroup>*/}
         </FormControl>
     );
 }
