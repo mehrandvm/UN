d3862077f783400efad8284cec8f8a014393b289
mehrandvm <amir.daneshvarmoein@gmail.com>
2020-10-28 07:59:46 +0330

    [UPD] updated image viewer with the new component

--- resources/js/components/case-details/ImageViewer.js
+++ resources/js/components/case-details/ImageViewer.js
@@ -1,51 +1,17 @@
-import React, { useState, useCallback } from 'react';
-import { render } from 'react-dom';
-import ImageViewer from 'react-simple-image-viewer';
-import noImage from '../../../images/no-image.png'
+import React from 'react'
+import ImgsViewer from 'react-images-viewer'
 
-function App() {
-    const [currentImage, setCurrentImage] = useState(0);
-    const [isViewerOpen, setIsViewerOpen] = useState(false);
-    const images = [
-        noImage
-    ];
-
-    const openImageViewer = useCallback((index) => {
-        setCurrentImage(index);
-        setIsViewerOpen(true);
-    }, []);
-
-    const closeImageViewer = () => {
-        setCurrentImage(0);
-        setIsViewerOpen(false);
-    };
-
+const ImageViewer = (props) => {
     return (
-        <div>
-            {/*{images.map((src, index) => (*/}
-            {/*    <img*/}
-            {/*        src={ src }*/}
-            {/*        onClick={ () => openImageViewer(index) }*/}
-            {/*        width="300"*/}
-            {/*        key={ index }*/}
-            {/*        style={{ margin: '2px' }}*/}
-            {/*        alt=""/>*/}
-            {/*))}*/}
-            <img
-                src={ images[0] }
-                onClick={ () => openImageViewer(0) }
-                key={ index }
-                style={{ margin: '2px', maxWidth: '100%', maxHeight: '100%', }}
-                alt=""/>
-            {isViewerOpen && (
-                <ImageViewer
-                    src={ images }
-                    currentIndex={ currentImage }
-                    onClose={ closeImageViewer }
-                />
-            )}
-        </div>
-    );
+        <ImgsViewer
+            imgs={props.imgs}
+            currImg={props.currImg}
+            isOpen={props.isOpen}
+            onClickPrev={props.gotoPrev}
+            onClickNext={props.gotoNext}
+            onClose={props.closeImgsViewer}
+        />
+    )
 }
 
-export default App
+export default ImageViewer
